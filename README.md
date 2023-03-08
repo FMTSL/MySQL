7c216efa14fde2dd241fa16322d4a65fc2aa6a37
Felipe Matos <felipe_matos@hotmail.com.br>
2023-03-08 16:18:22 -0300

    Handson - Create Database

---
+++ README.md
@@ -0,0 +1,53 @@
+<!---Criação de tabelas no MySQL-->
+create table tb_marcas (
+	id bigint auto_increment,
+    nome varchar (20) not null,
+    ativo boolean,
+    primary key(id)
+)
+
+<!---"foreign key" define a chave estrangeira, estando dentro dos parenteses a coluna e por fim fazendo referência outra tabela, colocando o nome da coluna entre parenteses-->
+create table tb_produtos (
+	id bigint auto_increment,
+    nome varchar (30) not null,
+    preco decimal,
+    marca_id bigint not null,
+    primary key (id),
+    foreign key (marca_id) references tb_marcas (id)
+)
+
+
+<!---Exclusão de tabelas no MySQL-->
+drop table tb_marcas
+
+<!---Exclusão coluna na tabela no MySQL-->
+delete from tb_marcas where id = 3
+
+<!---Atualização de coluna na tabela no MySQL-->
+update tb_produtos set preco = 79.80 where id = 3
+
+<!---Inserção de dados nas tabelas no MySQL-->
+insert into tb_marcas (nome, ativo) values ("Nike", true);
+insert into tb_marcas (nome, ativo) values ("H&M", true);
+insert into tb_marcas (nome, ativo) values ("Zara", true);
+insert into tb_marcas (nome, ativo) values ("Louis Vuitton", true);
+insert into tb_marcas (nome, ativo) values ("Adidas", true);
+insert into tb_marcas (nome, ativo) values ("Uniqlo", true);
+insert into tb_marcas (nome, ativo) values ("Hermes", true);
+insert into tb_marcas (nome, ativo) values ("Rolex", true);
+insert into tb_marcas (nome, ativo) values ("Gucci", true);
+insert into tb_marcas (nome, ativo) values ("Cartier", true);
+insert into tb_marcas (nome, ativo) values ("Polo", true);
+insert into tb_marcas (nome, ativo) values ("Armany", true);
+
+<!---Visualização de tabela completa no MySQL-->
+select * from tb_marcas
+
+<!---Visualização de coluna na tabela do MySQL-->
+select * from tb_produtos where marca_id =5
+
+select * from tb_produtos where marca_id =5 and nome like '%cal%'
+
+<!---Alteração coluna que já existe no MySQL-->
+<!---Determina o preço possui seis digitos antes da virgula e dois depois-->
+alter table tb_produtos modify column preco decimal (6,2)
---
+++ db_estoque.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `db_estoque` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_estoque`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_estoque
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_marcas`
+--
+
+DROP TABLE IF EXISTS `tb_marcas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_marcas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `ativo` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_produtos`
+--
+
+DROP TABLE IF EXISTS `tb_produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_produtos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `preco` decimal(6,2) DEFAULT NULL,
+  `marca_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `marca_id` (`marca_id`),
+  CONSTRAINT `tb_produtos_ibfk_1` FOREIGN KEY (`marca_id`) REFERENCES `tb_marcas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-08 16:13:19
